a791b080a80a6f33106a287cde6313184cb43b58
shinmiy <shinmiyaccs@gmail.com>
2019-05-09 15:33:24 +0900

    Implement image capture use case

--- app/src/main/java/com/shinmiy/cameraxdemo/MainActivity.kt
+++ app/src/main/java/com/shinmiy/cameraxdemo/MainActivity.kt
@@ -2,12 +2,20 @@ package com.shinmiy.cameraxdemo
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Matrix
 import android.os.Bundle
+import android.util.Log
+import android.util.Rational
+import android.util.Size
+import android.view.Surface
 import android.view.TextureView
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import java.io.File
 
 // This is an arbitrary number we are using to keep tab of the permission
 // request. Where an app has multiple context for requesting permission,
@@ -41,11 +49,77 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startCamera() {
-        // TODO: Implement CameraX operations
+        // Create configuration object for the viewfinder use case
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetAspectRatio(Rational(1, 1))
+            setTargetResolution(Size(640, 640))
+        }.build()
+
+        // Build the viewfinder use case
+        val preview = Preview(previewConfig)
+
+        // Every time the viewfinder is updated, recompute layout
+        preview.setOnPreviewOutputUpdateListener {
+            viewFinder.surfaceTexture = it.surfaceTexture
+            updateTransform()
+        }
+
+        // Create configuration object for the image capture use case
+        val imageCaptureConfig = ImageCaptureConfig.Builder()
+            .apply {
+                setTargetAspectRatio(Rational(1, 1))
+                // We don't set a resolution for image capture; instead, we
+                // select a capture mode which will infer the appropriate
+                // resolution based on aspect ration and requested mode
+                setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+            }.build()
+
+        // Build the image capture use case and attach button click listener
+        val imageCapture = ImageCapture(imageCaptureConfig)
+        findViewById<ImageButton>(R.id.capture_button).setOnClickListener {
+            val file = File(externalMediaDirs.first(), "${System.currentTimeMillis()}.jpg")
+            imageCapture.takePicture(file, object : ImageCapture.OnImageSavedListener {
+                override fun onImageSaved(file: File) {
+                    val msg = "Photo capture succeeded: ${file.absolutePath}"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d("CameraXApp", msg)
+                }
+
+                override fun onError(error: ImageCapture.UseCaseError, message: String, exc: Throwable?) {
+                    val msg = "Photo capture failed: $message"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.e("CameraXApp", msg)
+                    exc?.printStackTrace()
+                }
+            })
+        }
+
+        // Bind use cases to lifecycle
+        // If Android Studio complains about "this" being not a LifecycleOwner
+        // try rebuilding the project or updating the appcompat dependency to
+        // version 1.1.0 or higher.
+        CameraX.bindToLifecycle(this, preview, imageCapture)
     }
 
     private fun updateTransform() {
-        // TODO: Implement camera viewfinder transformations
+        val matrix = Matrix()
+
+        // Compute the center of the view finder
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
+
+        // Correct preview output to account for display rotation
+        val rotationDegrees = when (viewFinder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+
+        // Finally, apply transformations to our TextureView
+        viewFinder.setTransform(matrix)
     }
 
     /**
